e68595a41ca86cb2363eb83a56a92491fc8ad138
Localbug <fabian.dorner@arcor.de>
2019-01-07 20:44:03 +0100

    ArsenalHinzufügenScreen mit DBSpeicherung aus State erstellt

--- js/screens/ArsenalScreens/ArsenalHinzufügenScreen.js
+++ js/screens/ArsenalScreens/ArsenalHinzufügenScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {Button, SectionList, StyleSheet, Text, View } from 'react-native';
-import {erzeugeTabellen} from '../../../js/DataContext';
+import {Button, TextInput, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 import DBContext from '../../../js/DataContext';
+
 
 function SettingsItem(props) {
   return <Text style={styles.item}>{props.text}</Text>;
@@ -11,34 +11,104 @@ function SettingsHeader(props) {
   return <Text style={styles.section}>{props.text}</Text>;
 }
 
-function testfunc() {
-  //alert("Test");
+function DatenInDBSpeichern(_geschossDatensatz) {
+  const bgeschossDatensatz = new Object;
+  bgeschossDatensatz.bezeichnung = "bezeichnung";//this.state.bezeichnung;
+  bgeschossDatensatz.kaliber = "kaliber";//this.state.kaliber;
+  bgeschossDatensatz.gewicht = "gewicht";//this.state.gewicht;
+  bgeschossDatensatz.bc = "bc";//this.state.bc;
+  bgeschossDatensatz.preis = "preis";//this.state.preis;
+
+  console.log("speichere geschossdaten: "+JSON.stringify(_geschossDatensatz));
+  db = new DBContext();
+  db.speichereDatensatz('geschosse', _geschossDatensatz);
 }
 
 
 export default class ArsenalHinzufuegenScreen extends Component {
+  state = {
+    bezeichnung: '',
+    kaliber: '',
+    gewicht: '',
+    bc: '',
+    preis: ''
+  }
+
+  handleBezeichnung = (text) => {
+    this.setState({ bezeichnung: text })
+  }
+  handleKaliber = (text) => {
+      this.setState({ kaliber: text })
+  }
+  handleGewicht = (text) => {
+    this.setState({ gewicht: text })
+  }
+  handleBc = (text) => {
+      this.setState({ bc: text })
+  }
+  handlePreis = (text) => {
+    this.setState({ preis: text })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <SectionList
-          sections={[
-            {
-              title: 'ArsenalHinzufuegenScreen',
-              data: [{ key: '1', info: '1.0' }]
-            },
-            {
-              title: 'blabala',
-              data: [
-                { key: '2', info: 'Beispiel GmbH' },
-                { key: '3', info: 'copyright 2018' }
-              ]
-            }
-          ]}
-          renderItem={({ item }) => <SettingsItem text={item.info} />}
-          renderSectionHeader={({ section }) => (
-            <SettingsHeader text={section.title} />
-          )}
-        />
+        <View style = {styles.container}>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Name / Bezeichnung z.B: SMK 168 Moly"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleBezeichnung}/>
+            
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Kaliber z.B: .308Win"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleBezeichnung}/>
+                          
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Gewicht in Grain z.B: 168"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleKaliber}/>
+
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Balistik-Koeffizient z.B 0.420"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleBc}/>
+
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Preis in €/Stück z.B 0.48"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePreis}/>
+            
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress={() =>
+              {
+                //alert("Button geklickt...Hier soll DB angelegt und FakeDaten hinzugefügt werden")
+
+                const InterngeschossDatensatz = new Object;
+                InterngeschossDatensatz.bezeichnung = this.state.bezeichnung;
+                InterngeschossDatensatz.kaliber = this.state.kaliber;
+                InterngeschossDatensatz.gewicht = this.state.gewicht;
+                InterngeschossDatensatz.bc = this.state.bc;
+                InterngeschossDatensatz.preis = this.state.preis;
+
+                DatenInDBSpeichern(InterngeschossDatensatz);
+
+
+              }}>
+               <Text style = {styles.submitButtonText}> Hinzufügen </Text>
+            </TouchableOpacity>
+         </View>
 
         <View style= {{position: 'absolute', bottom:5, right:5}}>
           <Button
@@ -85,5 +155,20 @@ const styles = StyleSheet.create({
     color: 'dimgrey',
     fontSize: 18,
     padding: 5
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  },
+  submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+  },
+  submitButtonText:{
+      color: 'white'
   }
 });
